285aec0523dc7a7f1b13da2c8985adcae63be2bb
David Asensio Ortiz de Pinedo <davidasensio95@gmail.com>
2025-02-25 02:25:26 +0100

    PIOT-GDA-02-007

--- src/main/java/programmingtheiot/gda/system/SystemPerformanceManager.java
+++ src/main/java/programmingtheiot/gda/system/SystemPerformanceManager.java
@@ -33,6 +33,14 @@ public class SystemPerformanceManager
 	private static final Logger _Logger =Logger.getLogger(SystemPerformanceManager.class.getName());
 
 	private int pollRate =ConfigConst.DEFAULT_POLL_CYCLES;
+
+
+	private ScheduledExecutorService schedExecSvc = null;
+	private SystemCpuUtilTask sysCpuUtilTask = null;
+	private SystemMemUtilTask sysMemUtilTask = null;
+
+	private Runnable taskRunner = null;
+	private boolean isStarted = false;
 	
 	// constructors
 	
@@ -47,6 +55,14 @@ public class SystemPerformanceManager
 		if (this.pollRate <=0) {
 			this.pollRate =ConfigConst.DEFAULT_POLL_CYCLES;
 		}
+
+		this.schedExecSvc   = Executors.newScheduledThreadPool(1);
+		this.sysCpuUtilTask = new SystemCpuUtilTask();
+		this.sysMemUtilTask = new SystemMemUtilTask();
+
+		this.taskRunner = () -> {
+			this.handleTelemetry();
+		};
 	}
 	
 	
@@ -54,6 +70,11 @@ public class SystemPerformanceManager
 	
 	public void handleTelemetry()
 	{
+		float cpuUtil = this.sysCpuUtilTask.getTelemetryValue();
+		float memUtil = this.sysMemUtilTask.getTelemetryValue();
+
+		// NOTE: you may need to change the logging level to 'info' to see the message
+		_Logger.info("CPU utilization: " + cpuUtil + ", Mem utilization: " + memUtil);
 	}
 	
 	public void setDataMessageListener(IDataMessageListener listener)
@@ -63,13 +84,25 @@ public class SystemPerformanceManager
 
 	public boolean startManager()
 	{
-		_Logger.info("SystemPerformanceManager is starting...");
+		if (! this.isStarted) {
+			_Logger.info("SystemPerformanceManager is starting...");
 
-		return true;
+			ScheduledFuture<?> futureTask =
+					this.schedExecSvc.scheduleAtFixedRate(this.taskRunner, 1L, this.pollRate, TimeUnit.SECONDS);
+
+			this.isStarted = true;
+		} else {
+			_Logger.info("SystemPerformanceManager is already started.");
+		}
+
+		return this.isStarted;
 	}
 
 	public boolean stopManager()
 	{
+		this.schedExecSvc.shutdown();
+		this.isStarted = false;
+
 		_Logger.info("SystemPerformanceManager is stopped.");
 
 		return true;
