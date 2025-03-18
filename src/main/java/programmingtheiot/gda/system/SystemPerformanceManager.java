e431df9a38e1572f403cc92b8334d2e7870ce17e
David Asensio Ortiz de Pinedo <davidasensio95@gmail.com>
2025-03-18 02:19:53 +0100

    PIOT-GDA-05-002

--- src/main/java/programmingtheiot/gda/system/SystemPerformanceManager.java
+++ src/main/java/programmingtheiot/gda/system/SystemPerformanceManager.java
@@ -41,6 +41,9 @@ public class SystemPerformanceManager
 
 	private Runnable taskRunner = null;
 	private boolean isStarted = false;
+
+	private String locationID = ConfigConst.NOT_SET;
+	private IDataMessageListener dataMsgListener = null;
 	
 	// constructors
 	
@@ -63,22 +66,38 @@ public class SystemPerformanceManager
 		this.taskRunner = () -> {
 			this.handleTelemetry();
 		};
+
+		this.locationID = ConfigUtil.getInstance().getProperty(
+						ConfigConst.GATEWAY_DEVICE, ConfigConst.LOCATION_ID_PROP, ConfigConst.NOT_SET);
 	}
 	
 	
 	// public methods
-	
+
 	public void handleTelemetry()
 	{
 		float cpuUtil = this.sysCpuUtilTask.getTelemetryValue();
 		float memUtil = this.sysMemUtilTask.getTelemetryValue();
 
-		// NOTE: you may need to change the logging level to 'info' to see the message
+		// TODO: change the log level to 'info' for testing purposes
 		_Logger.info("CPU utilization: " + cpuUtil + ", Mem utilization: " + memUtil);
+
+		SystemPerformanceData spd = new SystemPerformanceData();
+		spd.setLocationID(this.locationID);
+		spd.setCpuUtilization(cpuUtil);
+		spd.setMemoryUtilization(memUtil);
+
+		if (this.dataMsgListener != null) {
+			this.dataMsgListener.handleSystemPerformanceMessage(
+					ResourceNameEnum.GDA_SYSTEM_PERF_MSG_RESOURCE, spd);
+		}
 	}
-	
+
 	public void setDataMessageListener(IDataMessageListener listener)
 	{
+		if (listener != null) {
+			this.dataMsgListener = listener;
+		}
 	}
 
 
