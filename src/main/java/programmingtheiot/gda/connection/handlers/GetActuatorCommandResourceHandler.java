e9c4d20d09178c762ca33603d3fd8d16de6a7161
David Asensio Ortiz de Pinedo <davidasensio95@gmail.com>
2025-05-28 21:42:47 +0200

    PIOT-GDA-08-003

---
+++ src/main/java/programmingtheiot/gda/connection/handlers/GetActuatorCommandResourceHandler.java
@@ -0,0 +1,127 @@
+/**
+ * This class is part of the Programming the Internet of Things project.
+ * 
+ * It is provided as a simple shell to guide the student and assist with
+ * implementation for the Programming the Internet of Things exercises,
+ * and designed to be modified by the student as needed.
+ */ 
+
+package programmingtheiot.gda.connection.handlers;
+
+import org.eclipse.californium.core.CoapResource;
+import org.eclipse.californium.core.coap.CoAP.ResponseCode;
+import org.eclipse.californium.core.server.resources.CoapExchange;
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
+
+import programmingtheiot.common.*;
+import programmingtheiot.data.DataUtil;
+import programmingtheiot.common.IDataMessageListener;
+import programmingtheiot.common.ResourceNameEnum;
+import programmingtheiot.data.ActuatorData;
+
+import java.util.logging.Logger;
+
+
+/**
+ * Shell representation of class for student implementation.
+ *
+ */
+public class GetActuatorCommandResourceHandler extends CoapResource
+		implements IActuatorDataListener
+{
+	// static
+
+	private static final Logger _Logger =
+		Logger.getLogger(GetActuatorCommandResourceHandler.class.getName());
+
+	// params
+	private ActuatorData actuatorData = null;
+
+	// constructors
+
+	/**
+	 * Constructor.
+	 *
+	 * @param resource Basically, the path (or topic)
+	 */
+	public GetActuatorCommandResourceHandler(ResourceNameEnum resource)
+	{
+		this(resource.getResourceName());
+	}
+
+	/**
+	 * Constructor.
+	 *
+	 * @param resourceName The name of the resource.
+	 */
+	public GetActuatorCommandResourceHandler(String resourceName)
+	{
+		super(resourceName);
+
+		// set the resource to be observable
+		super.setObservable(true);
+	}
+	
+	
+	// public methods
+
+
+	public boolean onActuatorDataUpdate(ActuatorData data) {
+		if (data != null && this.actuatorData != null) {
+			this.actuatorData.updateData(data);
+
+			// notify all connected clients
+			super.changed();
+
+			_Logger.fine("Actuator data updated for URI: " + super.getURI() + ": Data value = " + this.actuatorData.getValue());
+
+			return true;
+		}
+
+		return false;
+	}
+
+
+	
+	@Override
+	public void handleDELETE(CoapExchange context)
+	{
+	}
+	
+	@Override
+	public void handleGET(CoapExchange context)
+	{
+		// validate 'context'
+	    if (context == null) {
+	        _Logger.warning("CoapExchange context is null.");
+	        return;
+	    }
+
+		_Logger.info(
+				"GET request received for resource: " + super.getURI() + " with query: " + context.getRequestText());
+
+		// accept the request
+	    context.accept();
+
+		// Convert the locally stored ActuatorData to JSON using DataUtil
+	    String jsonData = DataUtil.getInstance().actuatorDataToJson(this.actuatorData);
+
+		// send an appropriate response
+	    context.respond(ResponseCode.CONTENT, jsonData, MediaTypeRegistry.APPLICATION_JSON);
+	}
+	
+	@Override
+	public void handlePOST(CoapExchange context)
+	{
+	}
+	
+	@Override
+	public void handlePUT(CoapExchange context)
+	{
+	}
+	
+	public void setDataMessageListener(IDataMessageListener listener)
+	{
+	}
+	
+}
